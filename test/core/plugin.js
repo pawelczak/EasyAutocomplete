1b9d2aa66dcd8bb1445ebfbd2603d5beb157d478
pawelczak <L.Pawelczak@icm.edu.pl>
2015-10-04 18:12:35 +0200

    Closes #86. Tests where input field has no id

--- test/core/plugin.js
+++ test/core/plugin.js
@@ -9,5 +9,36 @@ QUnit.test( "JQuery method exists", function( assert ) {
 	//assert
 	assert.ok($.fn.easyAutocomplete, "Method $.easyAutocomplete exists");
 	assert.ok($.fn.getSelectedItemIndex, "Method $.getSelectedItemIndex exists");
-	expect(2);
+	assert.ok($.fn.getItemData, "Method $.getItemData exists");
+	assert.ok($.fn.getSelectedItemData, "Method $.getSelectedItemData exists");
+	expect(4);
+});
+
+
+QUnit.test("Input field has no id property", function( assert ) {
+	
+	
+	//given
+	var completerOne = $(".inputOne").easyAutocomplete({
+
+		data: ["black", "white", "magenta", "yellow"],
+		
+	});
+
+
+	//execute
+	var e = $.Event('keyup');
+	e.keyCode = 50; 
+	$(".inputOne").val("more").trigger(e);
+
+	//assert
+	var elements = $(".inputOne").next().find("ul li");
+
+	assert.equal(4, elements.length, "Response size");
+	assert.equal("black", elements.eq(0).text(), "First element is 'black'");
+	assert.equal("white", elements.eq(1).text(), "Second element is 'white'");
+	assert.equal("magenta", elements.eq(2).text(), "Third element is 'magenta'");
+	assert.equal("yellow", elements.eq(3).text(), "Fourth element is 'yellow'");
+
+	expect(5);
 });
