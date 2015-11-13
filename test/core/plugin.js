3e29a8205fe47f7d92716648d4884014e005fcbc
pawelczak <L.Pawelczak@icm.edu.pl>
2015-11-13 23:22:35 +0100

    Closes #126. Test plugin for working with dynamic generated input fields.

--- test/core/plugin.js
+++ test/core/plugin.js
@@ -42,3 +42,40 @@ QUnit.test("Input field has no id property", function( assert ) {
 
 	expect(5);
 });
+
+QUnit.test("Invoke plugin function on input htat is dynamically added", function( assert ) {
+	
+	
+	//given
+
+	//create text field
+	var $dynamicInput = $("<input type='text' />").attr("id", "inputThree");
+
+	$(".inputOne").after($dynamicInput);
+
+	var completerOne = $("#inputThree").easyAutocomplete({
+
+		data: ["black", "white", "magenta", "yellow"],
+		
+	});
+
+
+	//execute
+	var e = $.Event('keyup');
+	e.keyCode = 50; 
+	$("#inputThree").val("more").trigger(e);
+
+	//assert
+	var elements = $("#inputThree").next().find("ul li");
+
+	assert.equal(4, elements.length, "Response size");
+	assert.equal("black", elements.eq(0).text(), "First element is 'black'");
+	assert.equal("white", elements.eq(1).text(), "Second element is 'white'");
+	assert.equal("magenta", elements.eq(2).text(), "Third element is 'magenta'");
+	assert.equal("yellow", elements.eq(3).text(), "Fourth element is 'yellow'");
+
+	expect(5);
+});
+
+
+
