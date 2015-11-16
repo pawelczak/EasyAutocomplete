d4ecdccf2c9ef4783e54dda0c2a34307ac80b916
pawelczak <L.Pawelczak@icm.edu.pl>
2015-11-16 20:11:06 +0100

    Bump version before release

--- dist/easy-autocomplete.themes.css
+++ dist/easy-autocomplete.themes.css
@@ -3,7 +3,7 @@
  * jQuery plugin for autocompletion
  * 
  * @author Łukasz Pawełczak (http://github.com/pawelczak)
- * @version 1.3.1
+ * @version 1.3.2
  * Copyright MIT License: https://github.com/pawelczak/easy-autocomplete/blob/master/LICENSE.txt
  */
 
